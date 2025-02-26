1f4144102fc9d81991219a089640b6eedd0a6986
GabrielCoelho <gbcoe@outlook.com.br>
2025-02-25 22:01:53 -0300

    feat(MenuApp): You can now run all in one
    `App` Class is now calling the exercises
    
    you can run: `mvn exec:java -Dexec.mainClass="br.com.devcoelho.App"` in
    the root of `aula03` directory.

--- aula03/src/main/java/br/com/devcoelho/App.java
+++ aula03/src/main/java/br/com/devcoelho/App.java
@@ -7,14 +7,21 @@ public class App {
   public static void main(String[] args) {
     Scanner entry = new Scanner(System.in);
     int op = 0;
-    while (op == 0) {
+    while (op != 9) {
       System.out.println("Escolha um exercício: ");
-      System.out.println("1. Acampamento");
+      System.out.println("1. Acampamento\n2. Computador\n3. Conta\n9. Sair");
       op = entry.nextInt();
       switch (op) {
         case 1:
           exercicio01();
-          op = 0;
+          break;
+        case 2:
+          exercicio02();
+          break;
+        case 3:
+          exercicio03();
+          break;
+        case 9:
           break;
 
         default:
@@ -32,5 +39,23 @@ public class App {
    */
   public static void exercicio01() {
     AcampamentoTeste.main();
+  }
+
+  /**
+   * Exercício 02 Descrição: Desenvolver uma classe Computador e implementar o método de impressão
+   * dos dados, um método para calcular um acrescimo em caso de duas marcas, e um método para
+   * alterar o valor diretamente (em caso do valor passado ser maior que zero).
+   */
+  public static void exercicio02() {
+    ComputadorTeste.main(null);
+  }
+
+  /**
+   * Exercício 03 Descrição: Desenvolver uma Classe e implementar seus atributos e métodos Noutra
+   * Classe, onde teremos o método main, e um objeto da primeira Classe "Conta"; Nela teremos o menu
+   * para integrar e trabalhar com os métodos da primeira classe.
+   */
+  public static void exercicio03() {
+    ContaTeste.main(null);
   }
 }
