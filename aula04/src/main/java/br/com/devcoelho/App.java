49d115fc2313d8ae1e524d70b1952815cd5bd6b4
GabrielCoelho <gbcoe@outlook.com.br>
2025-03-12 21:36:13 -0300

    feat: finalizando a modularização
    Projeto está quase finalizado, faltando poucas coisas.
    @TODO Documentar melhor os métodos
    @TODO manageAccount() method

--- aula04/src/main/java/br/com/devcoelho/App.java
+++ aula04/src/main/java/br/com/devcoelho/App.java
@@ -1,10 +1,51 @@
 package br.com.devcoelho;
 
-/**
- * Hello world!
- */
+import java.util.Scanner;
+
+/** Hello world! */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+  public static void main(String[] args) {
+    BankAdministrator manager = new BankAdministrator();
+    App appMain = new App();
+    Scanner sc = new Scanner(System.in);
+    int option = 0;
+    do {
+      appMain.appMenu();
+      option = sc.nextInt();
+      switch (option) {
+        case 1:
+          if (!manager.insertNewAccount()) {
+            System.out.println("Account couldn't be created.\nTry again!");
+          }
+          break;
+        case 2:
+          // manager.manageAccount();
+          break;
+        case 3:
+          if (!manager.deleteAccount()) {
+            System.out.println("There isn't any account active with that number.");
+          }
+          break;
+        case 4:
+          manager.consultAccount();
+          break;
+        case 0:
+          break;
+        default:
+          System.out.println("Option doesn't exists");
+          break;
+      }
+
+    } while (option != 0);
+    sc.close();
+  }
+
+  public void appMenu() {
+    System.out.println("Bank Management System");
+    System.out.println("1. Create Account");
+    System.out.println("2. Manage Account");
+    System.out.println("3. Remove Account");
+    System.out.println("4. Show Account");
+    System.out.println("0. Exit");
+  }
 }
--- aula04/src/main/java/br/com/devcoelho/BankAdministrator.java
+++ aula04/src/main/java/br/com/devcoelho/BankAdministrator.java
@@ -1,11 +1,13 @@
 package br.com.devcoelho;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 /** BankAdministrator */
 public class BankAdministrator {
-  private List<BankAccount> accountManaged;
+  private List<BankAccount> accountManaged = new ArrayList<>();
+  Scanner sc = new Scanner(System.in);
 
   public void consultAccount() {
     if (!accountManaged.isEmpty()) {
@@ -16,7 +18,6 @@ public class BankAdministrator {
   }
 
   public boolean insertNewAccount() {
-    Scanner sc = new Scanner(System.in);
     BankAccount account = new BankAccount();
     try {
       System.out.println("Inform the agency name: ");
@@ -25,15 +26,27 @@ public class BankAdministrator {
       account.setClientName(sc.nextLine());
       System.out.println("Inform the account number: ");
       account.setAccountNumber(sc.nextInt());
-      sc.close();
     } catch (Exception e) {
       System.out.println("Caught an error: " + e);
       return false;
     }
+    accountManaged.add(account);
     return true;
   }
 
   public boolean deleteAccount() {
+    System.out.println("Inform the account to be deleted: ");
+    int accountNumberChecked = sc.nextInt();
+    BankAccount account = new BankAccount();
+    for (BankAccount bankAccount : accountManaged) {
+      if (accountNumberChecked == bankAccount.getAccountNumber()) {
+        account = bankAccount;
+      }
+    }
+    if (accountManaged.contains(account)) {
+      accountManaged.remove(account);
+      return true;
+    }
 
     return false;
   }
